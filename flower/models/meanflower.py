98155c70cd51dd061e61269365da1e92c8c4e630
hedemil <emhed@kth.se>
2026-04-10 19:16:32 +0200

    .

--- flower/models/meanflower.py
+++ flower/models/meanflower.py
@@ -933,7 +933,7 @@ class MeanFlowerVLA(pl.LightningModule):
         """
         Encode proprioception based on action type.
         """
-        batch_size, _ = output_shape
+        batch_size = output_shape[0]
         default_dtype = next(self.parameters()).dtype
 
         if not self.use_proprio:
